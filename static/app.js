94075ffb9362f6c6204221aad6bec8477137b32e
Hemendra Chaudhary <hemendrachaudhary2052000@gmail.com>
2021-06-19 17:55:07 +0530

    Update app.js
    adding js functions

--- static/app.js
+++ static/app.js
@@ -32,3 +32,161 @@ function lw()
           
           
       }
+
+
+function checkDocker() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", `/checkDocker`, true);
+    xhr.send();
+    xhr.onload = function() {
+    var output = xhr.responseText;
+    document.getElementById("").innerHTML = output;
+    }
+}
+
+function startDocker() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", `/startDocker`, true);
+    xhr.send();
+    xhr.onload = function() {
+    var output = xhr.responseText;
+    document.getElementById("").innerHTML = output;
+    }
+}
+
+function stopDocker() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", `/stopDocker`, true);
+    xhr.send();
+    xhr.onload = function() {
+    var output = xhr.responseText;
+    document.getElementById("").innerHTML = output;
+    }
+}
+
+function dockerInfo() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", `/dockerInfo`, true);
+    xhr.send();
+    xhr.onload = function() {
+    var output = xhr.responseText;
+    document.getElementById("").innerHTML = output;
+    }
+}
+
+function listContainer() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", `/listContainer`, true);
+    xhr.send();
+    xhr.onload = function() {
+    var output = xhr.responseText;
+    document.getElementById("").innerHTML = output;
+    }
+}
+
+function listAllContainer() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", `/listAllContainer`, true);
+    xhr.send();
+    xhr.onload = function() {
+    var output = xhr.responseText;
+    document.getElementById("").innerHTML = output;
+    }
+}
+
+function listImages() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", `/listImages`, true);
+    xhr.send();
+    xhr.onload = function() {
+    var output = xhr.responseText;
+    document.getElementById("d1").innerHTML = output;
+    }
+}
+
+function runDocker() {
+    var xhr = new XMLHttpRequest();
+    var containerName = document.getElementsByName("containerName")[0].value;
+    var imageName = document.getElementsByName("imageName")[0].value;
+    var tagName = document.getElementsByName("tagName")[0].value;
+    xhr.open("GET", `/runDocker?containerName=${containerName}&imageName=${imageName}&tagName=${tagName}`, true);
+    xhr.send();
+    xhr.onload = function() {
+    var output = xhr.responseText;
+    document.getElementById("").innerHTML = output;
+    }
+}
+
+function startContainer() {
+    var xhr = new XMLHttpRequest();
+    var containerName = document.getElementsByName("containerName")[0].value;
+    // var containerId = document.getElementsByName("containerId")[0].value;
+    xhr.open("GET", `/startContainer?containerName=${containerName}`, true);
+    xhr.send();
+    xhr.onload = function() {
+    var output = xhr.responseText;
+    document.getElementById("").innerHTML = output;
+    }
+}
+
+function stopContainer() {
+    var xhr = new XMLHttpRequest();
+    var containerName = document.getElementsByName("containerName")[0].value;
+    // var containerId = document.getElementsByName("containerId")[0].value;
+    xhr.open("GET", `/stopContainer?containerName=${imageName}`, true);
+    xhr.send();
+    xhr.onload = function() {
+    var output = xhr.responseText;
+    document.getElementById("").innerHTML = output;
+    }
+}
+
+function execDocker() {
+    var xhr = new XMLHttpRequest();
+    var containerName = document.getElementsByName("containerName")[0].value;
+    var cmdName = document.getElementsByName("cmdName")[0].value;
+    // var containerId = document.getElementsByName("containerId")[0].value;
+    xhr.open("GET", `/execDocker?containerName=${containerName}&cmdName=${cmdName}`, true);
+    xhr.send();
+    xhr.onload = function() {
+    var output = xhr.responseText;
+    document.getElementById("").innerHTML = output;
+     }
+}
+
+function pullImage() {
+    var xhr = new XMLHttpRequest();
+    var imageName = document.getElementsByName("imageName")[0].value;
+    var tagName = document.getElementsByName("tagname")[0].value;
+    // var containerId = document.getElementsByName("containerId")[0].value;
+    xhr.open("GET", `/pullImage?imageName=${imageName}&tagname=${tagName}`, true);
+    xhr.send();
+    xhr.onload = function() {
+    var output = xhr.responseText;
+    document.getElementById("").innerHTML = output;
+     }
+}
+
+function removeImage() {
+    var xhr = new XMLHttpRequest();
+    var imageId = document.getElementsByName("imageId")[0].value;
+    xhr.open("GET", `/removeImage?imageId=${imageId}`, true);
+    xhr.send();
+    xhr.onload = function() {
+    var output = xhr.responseText;
+    document.getElementById("").innerHTML = output;
+    }
+}
+
+function removeContainer() {
+    var xhr = new XMLHttpRequest();
+    var containerName = document.getElementsByName("containerName")[0].value;
+    // var containerId = document.getElementsByName("containerId")[0].value;
+    xhr.open("GET", `/removeContainer?containerName=${containerName}`, true);
+    xhr.send();
+    xhr.onload = function() {
+    var output = xhr.responseText;
+    document.getElementById("").innerHTML = output;
+    }
+}
+
